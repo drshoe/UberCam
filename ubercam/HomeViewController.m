a23e932116125612535961c3123544a64dd5d893
Sheng Xu <xusheng@rogers.com>
2014-02-25 09:58:31 -0500

    implemented section related methods

--- ubercam/HomeViewController.m
+++ ubercam/HomeViewController.m
@@ -28,7 +28,7 @@
     self = [super initWithCoder:aDecoder];
     if (self) {
         // This table displays items in the Todo class
-        self.parseClassName = @"TestObject";
+        self.parseClassName = @"Photo";
         self.pullToRefreshEnabled = YES;
         self.paginationEnabled = YES;
         self.objectsPerPage = 3;
@@ -51,6 +51,71 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - PFQueryTableViewDataSource and Delegates
+- (void)objectsDidLoad:(NSError *)error {
+    [super objectsDidLoad:error];
+    
+}
+
+// return objects in a different indexpath order. in this case we return object based on the section, not row, the default is row
+- (PFObject *)objectAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    static NSString *CellIdentifier = @"SectionHeaderCell";
+    UITableViewCell *sectionHeaderView = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+    
+    PFImageView *profileImageView = (PFImageView *)[sectionHeaderView viewWithTag:1];
+    UILabel *userNameLabel = (UILabel *)[sectionHeaderView viewWithTag:2];
+    UILabel *titleLabel = (UILabel *)[sectionHeaderView viewWithTag:3];
+    
+    PFObject *photo = [self.objects objectAtIndex:section];
+    PFUser *user = [photo objectForKey:@"whoTook"];
+    PFFile *profilePicture = [user objectForKey:@"profilePicture"];
+    NSString *title = photo[@"title"];
+    
+    userNameLabel.text = user.username;
+    titleLabel.text = title;
+    
+    profileImageView.file = profilePicture;
+    [profileImageView loadInBackground];
+}
+
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    NSInteger sections = self.objects.count;
+    return sections;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath object:(PFObject *)object {
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForNextPageAtIndexPath:(NSIndexPath *)indexPath {
+}
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+}
+
+- (PFQuery *)queryForTable {
+    
 }
 
 @end
