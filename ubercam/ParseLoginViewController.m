f7fad960272c22fc35c575fe02110de869032cec
Sheng Xu <xusheng@rogers.com>
2014-02-28 14:06:29 -0500

    adds login UI

--- ubercam/ParseLoginViewController.m
+++ ubercam/ParseLoginViewController.m
@@ -27,6 +27,9 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.logInView.backgroundColor = BLUE_COLOR;
+    self.logInView.logo = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"logo"]];
+    [self.logInView.facebookButton setTitle:@"Login with Facebook" forState:UIControlStateNormal];
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +37,49 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+    self.logInView.facebookButton.imageEdgeInsets = UIEdgeInsetsMake(0, 0, 0, 20);
+    
+    CGRect frame = self.logInView.logo.frame;
+    frame.origin.y = 150;
+    self.logInView.logo.frame = frame;
+    frame = self.logInView.facebookButton.frame;
+    frame.origin.y = 300;
+    self.logInView.facebookButton.frame = frame;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
