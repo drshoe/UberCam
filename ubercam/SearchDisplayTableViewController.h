5f4a0a360da409b8db86dd2caee7195b5ed0f5d6
Daniel Xu <dxu@keek.com>
2014-10-21 17:32:39 -0400

    add search bar and Facebook sdk

---
+++ ubercam/SearchDisplayTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchDisplayTableViewController.h
+//  ubercam
+//
+//  Created by Daniel Xu on 2014-10-21.
+//  Copyright (c) 2014 danielxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+@interface SearchDisplayTableViewController : PFQueryTableViewController <UISearchControllerDelegate>
+
+@end
